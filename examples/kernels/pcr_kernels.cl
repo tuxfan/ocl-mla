ecc1ce4a8b8890051ac43d0d15a6c4ed936aa2ea
Ben Bergen <bergen@lanl.gov>
2012-05-23 11:37:14 -0600

    Added tri-diagonal test.

---
+++ examples/kernels/pcr_kernels.cl
@@ -0,0 +1,135 @@
+/*
+ * Copyright 1993-2010 NVIDIA Corporation.  All rights reserved.
+ *
+ * Please refer to the NVIDIA end user license agreement (EULA) associated
+ * with this source code for terms and conditions that govern your use of
+ * this software. Any use, reproduction, disclosure, or distribution of
+ * this software and related documentation outside the terms of the EULA
+ * is strictly prohibited.
+ *
+ */
+ 
+ /*
+ * Copyright 1993-2010 NVIDIA Corporation.  All rights reserved.
+ * 
+ * Tridiagonal solvers.
+ * Device code for parallel cyclic reduction (PCR).
+ *
+ * Original CUDA kernels: UC Davis, Yao Zhang & John Owens, 2009
+ * 
+ * NVIDIA, Nikolai Sakharnykh, 2009
+ */
+
+//#define NATIVE_DIVIDE
+
+#pragma OPENCL EXTENSION cl_khr_fp64 : enable
+#pragma OPENCL EXTENSION cl_amd_printf : enable
+
+__kernel void pcr_branch_free_kernel(__global real_t *a_d,
+	__global real_t *b_d, __global real_t *c_d, __global real_t *d_d,
+	__global real_t *x_d, __local real_t *shared, int system_size,
+	int num_systems, int iterations) {
+	size_t thid = get_local_id(0);
+	size_t blid = get_group_id(0);
+//	size_t grps = get_num_groups(0);
+//	size_t lsize = get_local_size(0);
+//	size_t gsize = get_global_size(0);
+
+//	printf("lsize: %d\n", (int)lsize);
+//	printf("thid: %d\n", (int)thid);
+//	printf("grps: %d\n", (int)grps);
+//	printf("gsize: %d\n", (int)gsize);
+//	printf("blid: %d\n", (int)blid);
+
+	int delta = 1;
+
+	__local real_t * a = shared;
+	__local real_t * b = &a[system_size+1];
+	__local real_t * c = &b[system_size+1];
+	__local real_t * d = &c[system_size+1];
+	__local real_t * x = &d[system_size+1];
+
+	a[thid] = a_d[thid + blid * system_size];
+	b[thid] = b_d[thid + blid * system_size];
+	c[thid] = c_d[thid + blid * system_size];
+	d[thid] = d_d[thid + blid * system_size];
+
+//	printf("system_size: %d\n", system_size);
+//	printf("num_systems: %d\n", num_systems);
+//	printf("iterations: %d\n", iterations);
+
+// printf("a: %f\n", a[thid]);
+// printf("b: %f\n", b[thid]);
+// printf("c: %f\n", c[thid]);
+// printf("d: %f\n", d[thid]);
+  
+	real_t aNew, bNew, cNew, dNew;
+  
+	barrier(CLK_LOCAL_MEM_FENCE);
+
+	// parallel cyclic reduction
+	for (int j = 0; j < iterations; j++) {
+		int i = thid;
+
+		int iRight = i+delta;
+		iRight = iRight & (system_size-1);
+
+		int iLeft = i-delta;
+		iLeft = iLeft & (system_size-1);
+
+#ifndef NATIVE_DIVIDE
+		real_t tmp1 = a[i] / b[iLeft];
+		real_t tmp2 = c[i] / b[iRight];
+#else
+		real_t tmp1 = native_divide(a[i], b[iLeft]);
+		real_t tmp2 = native_divide(c[i], b[iRight]);
+#endif
+
+		bNew = b[i] - c[iLeft] * tmp1 - a[iRight] * tmp2;
+		dNew = d[i] - d[iLeft] * tmp1 - d[iRight] * tmp2;
+		aNew = -a[iLeft] * tmp1;
+		cNew = -c[iRight] * tmp2;
+
+		barrier(CLK_LOCAL_MEM_FENCE);
+        
+		b[i] = bNew;
+ 		d[i] = dNew;
+		a[i] = aNew;
+		c[i] = cNew;	
+    
+		delta *= 2;
+		barrier(CLK_LOCAL_MEM_FENCE);
+	} // for
+
+	if (thid < delta) {
+		int addr1 = thid;
+		int addr2 = thid + delta;
+		real_t tmp3 = b[addr2] * b[addr1] - c[addr1] * a[addr2];
+#ifndef NATIVE_DIVIDE
+		x[addr1] = (b[addr2] * d[addr1] - c[addr1] * d[addr2]) / tmp3;
+		x[addr2] = (d[addr2] * b[addr1] - d[addr1] * a[addr2]) / tmp3;
+#else
+		x[addr1] = native_divide((b[addr2] * d[addr1] - c[addr1] *
+			d[addr2]), tmp3);
+		x[addr2] = native_divide((d[addr2] * b[addr1] - d[addr1] *
+			a[addr2]), tmp3);
+#endif
+	} // if
+    
+	barrier(CLK_LOCAL_MEM_FENCE);
+    
+	x_d[thid + blid * system_size] = x[thid];
+//	printf("x: %f\n", x[thid]);
+} // pcr_branch_free_kernel
+
+/*
+ * Local Variables:
+ * mode: c
+ * c-basic-offset:3
+ * c-file-offsets: ((arglist-intro . +))
+ * indent-tabs-mode:t
+ * tab-width:3
+ * End:
+ *
+ * vim: set syntax=c : set ts=3 :
+ */
---
+++ examples/tridiag.c
@@ -0,0 +1,167 @@
+/*----------------------------------------------------------------------------*
+ *----------------------------------------------------------------------------*/
+
+#include <stdio.h>
+#include <math.h>
+
+#include <ocl.h>
+#include <example_strings.h>
+
+#if 0
+	typedef double real_t;
+	const char * COMPILE_OPTS = "-Dreal_t=double";
+#else
+	typedef float real_t;
+	const char * COMPILE_OPTS = "-Dreal_t=float";
+#endif
+
+const real_t PI = 3.14159;
+const size_t k = 1;
+
+#define SQR(x) (x)*(x)
+#define ABS(x, y) (x)-(y) > 0 ? (x)-(y) : (y)-(x)
+
+int main(int argc, char ** argv) {
+
+	if(argc != 2) {
+		fprintf(stderr, "Usage: %s <elements>\n", argv[0]);
+		exit(1);
+	} // if
+
+	size_t elements = atoi(argv[1]);
+
+	int32_t systems = elements*256;
+
+	real_t * h_sub = (real_t *)malloc(systems*elements*sizeof(real_t));
+	real_t * h_diag = (real_t *)malloc(systems*elements*sizeof(real_t));
+	real_t * h_sup = (real_t *)malloc(systems*elements*sizeof(real_t));
+	real_t * h_rhs = (real_t *)malloc(systems*elements*sizeof(real_t));
+	real_t * h_x = (real_t *)malloc(systems*elements*sizeof(real_t));
+	real_t * analytic = (real_t *)malloc(systems*elements*sizeof(real_t));
+	real_t x0 = 0.0;
+	real_t x1 = 1.0;
+	real_t h = (x1-x0)/(real_t)(elements-1);
+	real_t hinv2 = 1.0/SQR(h);
+	int32_t iterations = 0;
+
+	cl_mem d_sub;
+	cl_mem d_diag;
+	cl_mem d_sup;
+	cl_mem d_rhs;
+	cl_mem d_x;
+
+	size_t i, s;
+
+	for(s=0; s<systems; ++s) {
+		for(i=0; i<elements; ++i) {
+			h_sub[s*elements + i] = hinv2*(-1.0);	
+			h_diag[s*elements + i] = hinv2*(2.0);	
+			h_sup[s*elements + i] = hinv2*(-1.0);	
+
+			analytic[s*elements + i] = 1.0;
+			h_rhs[s*elements + i] = (i==0 || i==(elements-1)) ? 1.0 : 0.0;
+
+			h_x[s*elements + i] = 0.0;
+		} // for
+
+		h_sub[s*elements] = 0.0;
+		h_sup[s*elements + elements-1] = 0.0;
+	} // for
+
+	iterations = elements/2;
+	int32_t cnt = 1;
+
+	do {
+		++cnt;
+		iterations/=2;
+	} while (iterations > 1);
+
+	iterations = cnt-1;
+
+	ocl_init();
+
+	ocl_add_program(OCL_PERFORMANCE_DEVICE, "tridiag", pcr_kernels_PPSTR,
+		COMPILE_OPTS);
+	ocl_add_kernel(OCL_PERFORMANCE_DEVICE, "tridiag",
+		"pcr_branch_free_kernel", "solve");
+
+	ocl_kernel_hints_t kernel_hints;
+	ocl_kernel_hints("tridiag", "solve", &kernel_hints);
+
+	if(elements > kernel_hints.max_work_group_size) {
+		fprintf(stderr, "Error: too many elements for device!!!");
+		exit(1);
+	} // if
+
+	size_t global_offset = 0;
+	size_t local_size;
+	size_t work_group_indeces;
+	size_t single_indeces;
+
+	ocl_ndrange_hints(elements, kernel_hints.max_work_group_size,
+		0.5, 0.5, &local_size, &work_group_indeces, &single_indeces);
+
+	ocl_create_buffer(OCL_PERFORMANCE_DEVICE, systems*elements*sizeof(real_t),
+		CL_MEM_READ_ONLY | CL_MEM_COPY_HOST_PTR, h_sub, &d_sub);
+	ocl_create_buffer(OCL_PERFORMANCE_DEVICE, systems*elements*sizeof(real_t),
+		CL_MEM_READ_ONLY | CL_MEM_COPY_HOST_PTR, h_diag, &d_diag);
+	ocl_create_buffer(OCL_PERFORMANCE_DEVICE, systems*elements*sizeof(real_t),
+		CL_MEM_READ_ONLY | CL_MEM_COPY_HOST_PTR, h_sup, &d_sup);
+	ocl_create_buffer(OCL_PERFORMANCE_DEVICE, systems*elements*sizeof(real_t),
+		CL_MEM_READ_ONLY | CL_MEM_COPY_HOST_PTR, h_rhs, &d_rhs);
+	ocl_create_buffer(OCL_PERFORMANCE_DEVICE, systems*elements*sizeof(real_t),
+		CL_MEM_WRITE_ONLY, NULL, &d_x);
+
+	ocl_set_kernel_arg("tridiag", "solve", 0, sizeof(cl_mem), &d_sub);
+	ocl_set_kernel_arg("tridiag", "solve", 1, sizeof(cl_mem), &d_diag);
+	ocl_set_kernel_arg("tridiag", "solve", 2, sizeof(cl_mem), &d_sup);
+	ocl_set_kernel_arg("tridiag", "solve", 3, sizeof(cl_mem), &d_rhs);
+	ocl_set_kernel_arg("tridiag", "solve", 4, sizeof(cl_mem), &d_x);
+	ocl_set_kernel_arg("tridiag", "solve", 5,
+		(elements+1)*5*sizeof(real_t), NULL);
+	ocl_set_kernel_arg("tridiag", "solve", 6, sizeof(int32_t), &elements);
+	ocl_set_kernel_arg("tridiag", "solve", 7, sizeof(int32_t), &systems);
+	ocl_set_kernel_arg("tridiag", "solve", 8, sizeof(int32_t), &iterations);
+
+	ocl_event_t event;
+
+	ocl_initialize_event(&event);
+
+	size_t global_size = systems*elements;
+
+	ocl_enqueue_kernel_ndrange(OCL_PERFORMANCE_DEVICE, "tridiag", "solve",
+		1, &global_offset, &global_size, &local_size, &event);
+
+	ocl_finish(OCL_PERFORMANCE_DEVICE);
+
+	ocl_add_timer("solve", &event);
+
+	ocl_enqueue_read_buffer(OCL_PERFORMANCE_DEVICE, d_x, 1, global_offset,
+		systems*elements*sizeof(real_t), h_x, &event);
+
+	ocl_add_timer("read", &event);
+
+	double rms = 0.0;
+	for(i = 0; i<systems*elements; ++i) {
+		real_t abs = ABS(analytic[i], h_x[i]);
+		rms += SQR(abs);
+	} // for
+
+	rms /= elements;
+	rms = sqrt(rms);
+
+	fprintf(stdout, "rms: %lf\n", rms);
+
+	ocl_report_timer("solve");
+	ocl_report_timer("read");
+
+	ocl_finalize();
+
+	free(h_sub);
+	free(h_diag);
+	free(h_sup);
+	free(h_rhs);
+	free(h_x);
+
+	return 0;
+} // main
